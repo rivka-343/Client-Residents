e94c168f31c1d29d2bc3293d9b84bb5affb76d57
rivka-343 <rivka747343@gmail.com>
2025-03-21 02:09:57 +0200

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../environments/environment';
 export class AuthService {
   // private apiUrl = "https://localhost:7126/api";
  // private apiUrl = "https://server-property-tax.onrender.com/api"
-  private apiUrl = process.env.apiUrl || 'http://localhost:3000';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
   login(userName: string, password: string): Observable<{ token: string; role: string }> {
