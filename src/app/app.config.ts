a316854be2bdae6c1a6ccadbd49d4957cdf4aa00
Rivky <rivka747343@gmail.com>
2025-03-24 22:50:55 +0200

    add prerender

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(), 
-    provideClientHydration(withEventReplay())]
+ 
+    // provideHttpClient(), 
+    // provideClientHydration(withEventReplay())
+    provideClientHydration(),provideHttpClient(),
+    // provideAnimationsAsync()
+  ],
+
 };
