10517af5b59d96dbb29b58e940bc1294b2bf6ac5
Rivky <rivka747343@gmail.com>
2025-03-30 23:46:18 +0300

    ----

--- src/app/components/RequestData.ts
+++ src/app/components/RequestData.ts
@@ -1,17 +1,21 @@
-export  interface DocumentUpload {
-    fileName: string;
-    contentType: string;
-    s3Url: string;
-    type: number; // DocumentType
-  }
-  
+export interface DocumentUpload {
+  fileName: string;
+  contentType: string;
+  s3Url: string;
+  type: number; // DocumentType
+}
+
 export interface RequestData {
-    fName: string;
-    lfName: string;
-    gmail: string;
-    homeNumber: string;
-    street: string;
-    city: string;
-    propertyNumber: string;
-    documentUploads: DocumentUpload[];
-  }
+  fName: string;
+  lfName: string;
+  gmail: string;
+  homeNumber: string;
+  street: string;
+  city: string;
+  propertyNumber: string;
+  status: string;
+  averageMonthlyIncome: number;
+  calculatedArnona: number;
+  approvedArnona: number;
+  documentUploads: DocumentUpload[];
+}
